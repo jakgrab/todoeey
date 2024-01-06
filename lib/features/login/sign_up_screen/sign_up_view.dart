b0dd7b821e9dc2324270406ec15d825c7081256f
jakub.grabowski <kubagrabowski1903@gmail.com>
2024-01-06 16:55:32 +0100

    Add signup screen

--- lib/features/login/sign_up_screen/sign_up_view.dart
+++ lib/features/login/sign_up_screen/sign_up_view.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:todoeey/common/constants/app_colors.dart';
 import 'package:todoeey/common/constants/route_paths.dart';
-import 'package:todoeey/common/extensions/navigation/navigation_extension.dart';
 import 'package:todoeey/common/text_styles/todoeey_text_styles.dart';
+import 'package:todoeey/common/widgets/todoeey_app_bar.dart';
 import 'package:todoeey/common/widgets/todoeey_button.dart';
-import 'package:todoeey/features/login/sign_up_screen/sign_up_screen.dart';
 
 import '../widgets/todoeey_text_field.dart';
 
-class LoginView extends StatelessWidget {
-  const LoginView({super.key});
+class SignUpView extends StatelessWidget {
+  const SignUpView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: const TodoeeyAppBar(showSimplifiedVersion: true),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
@@ -22,12 +21,11 @@ class LoginView extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const SizedBox(height: 84),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
                     Text(
-                      "Login",
+                      "Sign Up",
                       style: TodoeeyTextStyle.h1(),
                     ),
                   ],
@@ -39,7 +37,7 @@ class LoginView extends StatelessWidget {
                   // errorMessage: "wrong",
                   onChanged: (value) {},
                 ),
-                const SizedBox(height: 45),
+                const SizedBox(height: 35),
                 TodoeeyTextField(
                   labelText: "Password",
                   keyboardType: TextInputType.emailAddress,
@@ -47,18 +45,19 @@ class LoginView extends StatelessWidget {
                   isPassword: true,
                   onChanged: (value) {},
                 ),
-                const SizedBox(height: 55),
-                TodoeeyButton(
-                  text: "Log In ",
-                  onClick: () => Modular.to.pushNamed(RoutePaths.dashboardModule.modulePath),
+                const SizedBox(height: 35),
+                TodoeeyTextField(
+                  labelText: "Repeated Password",
+                  keyboardType: TextInputType.emailAddress,
+                  // errorMessage: "wrong",
+                  isPassword: true,
+                  onChanged: (value) {},
                 ),
-                const SizedBox(height: 25),
+                const SizedBox(height: 55),
                 TodoeeyButton(
                   text: "Sign Up",
-                  color: AppColors.secondaryGrey,
-                  onClick: () => Modular.to.pushNamed(SignUpScreen.path.relativePath),
+                  onClick: () => Modular.to.pushNamed(RoutePaths.dashboardModule),
                 ),
-                const SizedBox(height: 55),
               ],
             ),
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:todoeey/app.dart';
 import 'package:todoeey/app_module.dart';
+import 'package:todoeey/common/constants/app_colors.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,6 +11,14 @@ void main() {
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
+
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle.light.copyWith(
+      systemNavigationBarColor: AppColors.background,
+      statusBarColor: Colors.transparent,
+      systemStatusBarContrastEnforced: false,
+    ),
+  );
 
   runApp(ModularApp(
     module: AppModule(),
