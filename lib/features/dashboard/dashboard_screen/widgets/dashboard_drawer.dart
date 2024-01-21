6855f9680a1baea43bbdb9937326099ca630cdcc
jakub.grabowski <kubagrabowski1903@gmail.com>
2024-01-21 22:48:49 +0100

    Allow signing out

--- lib/features/dashboard/dashboard_screen/widgets/dashboard_drawer.dart
+++ lib/features/dashboard/dashboard_screen/widgets/dashboard_drawer.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 import '../../../../common/text_styles/todoeey_text_styles.dart';
+import '../../../../repositories/auth/auth_repository_interface.dart';
 
 class DashboardDrawer extends StatelessWidget {
   const DashboardDrawer({super.key});
@@ -14,8 +16,13 @@ class DashboardDrawer extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 30),
             child: TextButton(
-              onPressed: () {
-                Scaffold.of(context).closeEndDrawer();
+              onPressed: () async {
+                final authRepository = Modular.get<AuthRepositoryInterface>();
+                await authRepository.signOut();
+
+                if (context.mounted) {
+                  Scaffold.of(context).closeEndDrawer();
+                }
               },
               child: Text(
                 'Log out',
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
@@ -14,14 +12,6 @@ import 'data/hive_entities/note/note_entity.dart';
 import 'data/hive_entities/user_credentials/user_credentials_entity.dart';
 import 'data/hive_entities/user_notes/user_notes_entity.dart';
 import 'services/shared_preferences/shared_preferences_service.dart';
-
-// class MyHttpOverrides extends HttpOverrides {
-//   @override
-//   HttpClient createHttpClient(SecurityContext? context) {
-//     return super.createHttpClient(context)
-//       ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
-//   }
-// }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -40,8 +30,6 @@ void main() async {
 
   await _setInitialRoute();
   await _initHiveDatabase();
-
-  // HttpOverrides.global = MyHttpOverrides();
 
   runApp(ModularApp(
     module: AppModule(),
